a75c1108b0e676781c954420622f43a945598b0a
techindiesoft <officialkarategamer@gmail.com>
2023-06-06 19:45:38 +0530

    this is my first commit for c programe

---
+++ inputdata.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main(){
+
+int i,j,k;
+printf("enter three numbers:");
+scanf("%d,%d,%d",& i,j,k);
+printf("you enterd %d,%d,%dz",i,j,k);
+
+
+
+ }
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    // Displays the string inside quotations
+    printf("HELLO EVERYONE\n");
+    scanf("%d", & n);
+    return 0;
+    
+}
